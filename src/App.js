0f37f1b541231a23389227af615dc4d2f30f2437
Adlet <iadl2764@gmail.com>
2023-12-22 20:47:55 +0600

    to

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 const App = () => {
   return (
     <div className="App">
-      <h1>Mobi
-      </h1>
+      <h1> MobiMarket </h1>
+      <div className='app'> 
+        <input placeholder='Username'/>
+      </div>
     </div>
   );
 }
-
 
 
 
